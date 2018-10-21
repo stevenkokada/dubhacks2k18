43b471a88ec5af923eb90d9193b12c99e240836c
amyxu1 <amyxu2002@gmail.com>
2018-10-21 03:17:51 -0700

    Added child labor website.

--- extension_template/display_report.js
+++ extension_template/display_report.js
@@ -7,7 +7,6 @@ chrome.storage.sync.get("currency-type", function(result) {
   if (conversion_type == 'lbs of rice' || conversion_type == 'meals' || conversion_type == 'weeks of groceries'){
     window.open("https://give.wfp.org/607/?step=country&utm_campaign=2017-wfp-donatenowpage-onetime-button1&utm_source=2017-wfp-donatenowpage-onetime-button1&_ga=2.16435313.654337630.1540113451-137370510.1540113451&utm_medium=legacy");
 
-
   }
 
   if (conversion_type == 'gals of water'){
@@ -19,6 +18,10 @@ chrome.storage.sync.get("currency-type", function(result) {
   if (conversion_type == 'malaria nets'){
     window.open("https://www.againstmalaria.com/donate.aspx");
 
+  }
+
+  if (conversion_type = 'days of child labor'){
+    window.open("http://endchildlabor.net/donate/");
   }
 
 });
