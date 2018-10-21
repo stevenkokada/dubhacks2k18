46a7d0e87f365d359dbe8c06d42451420b75dbcc
Steven Okada <stevenkokada@gmail.com>
2018-10-20 19:57:49 -0700

    recursive solution included

--- extension_template/content.js
+++ extension_template/content.js
@@ -1,32 +1,62 @@
 var conversion = {"bags of rice": 300}
+// var elements = document.getElementsByTagName('*');
+//
+// for (var i = 0; i < elements.length; i++) {
+//     var element = elements[i];
+//
+//     for (var j = 0; j < element.childNodes.length; j++) {
+//         var node = element.childNodes[j];
+//
+//         if (true) {
+//             var text = node.nodeValue;
+//             var r = /^\$[0-9]+\.?[0-9]?[0-9]?$/
+//             var original = text.match(r);
+//
+//             if (original != null){
+//
+//
+//             var numeric = parseFloat(original.toString().substring(1)) * conversion["bags of rice"]
+//
+//
+//
+//             var replacedText = text.replace(r, numeric.toString() + " bags of rice");
+//
+//             if (replacedText !== text) {
+//                 element.replaceChild(document.createTextNode(replacedText), node);
+//             }
+//
+//           }
+//         }
+//     }
+// }
+//
 
-var elements = document.getElementsByTagName('*');
+function walkText(node) {
 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+  var conversion = {"bags of rice": 300}
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
+  if (node.nodeType == 3) {
+    var text = node.nodeValue;
+    var r = /^\$?[0-9]+(\.[0-9][0-9])?$/;
+    var original = text.match(r);
 
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var r = /^\$?[0-9]+(\.[0-9][0-9])?$/;
-            var original = text.match(r);
-
-            if (original != null){
+    if (original != null){
 
 
-            var numeric = parseFloat(original.toString().substring(1)) * conversion["bags of rice"]
+    var numeric = parseFloat(original.toString().substring(1)) * conversion["bags of rice"]
+    var replacedText = text.replace(r, numeric.toString() + " bags of rice");
 
-
-
-            var replacedText = text.replace(r, numeric.toString() + " bags of rice");
-
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
-
-          }
-        }
+    if (replacedText !== text) {
+        element.replaceChild(document.createTextNode(replacedText), node);
     }
+  }
 }
+
+
+  if (node.nodeType == 1 && node.nodeName != "SCRIPT") {
+    for (var i = 0; i < node.childNodes.length; i++) {
+      walkText(node.childNodes[i]);
+    }
+  }
+}
+walkText(document.body);
