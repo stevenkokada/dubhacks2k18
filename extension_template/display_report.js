23aca002b6593090113931bc128498a9754a948e
Steven Okada <stevenkokada@gmail.com>
2018-10-21 09:27:06 -0700

    Updated matches to include top 10 shopping websites + Updated metrics

--- extension_template/display_report.js
+++ extension_template/display_report.js
@@ -4,7 +4,7 @@ $("#add-to-cart-button").click(function(){
 chrome.storage.sync.get("currency-type", function(result) {
   var conversion_type = result['currency-type'];
 
-  if (conversion_type == 'lbs of rice' || conversion_type == 'meals' || conversion_type == 'weeks of groceries'){
+  if (conversion_type == 'lbs of rice' || conversion_type == 'meals' || conversion_type == 'weeks of groceries' || conversion_type == 'big macs'){
     window.open("https://give.wfp.org/607/?step=country&utm_campaign=2017-wfp-donatenowpage-onetime-button1&utm_source=2017-wfp-donatenowpage-onetime-button1&_ga=2.16435313.654337630.1540113451-137370510.1540113451&utm_medium=legacy");
 
   }
@@ -23,6 +23,7 @@ chrome.storage.sync.get("currency-type", function(result) {
   if (conversion_type == 'days of child labor'){
     window.open("http://endchildlabor.net/donate/");
   }
+
 
 });
 
