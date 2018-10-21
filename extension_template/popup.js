c9b02c0d8ea046b8e15ee2b5578b15ade8744554
amyxu1 <amyxu2002@gmail.com>
2018-10-21 00:59:37 -0700

    Added comments

--- extension_template/popup.js
+++ extension_template/popup.js
@@ -1,8 +1,10 @@
 var changeType = function() {
 	var currencyType = document.getElementById('currencyType').value;
+	// sets the current alternative altruistic currency type
 	chrome.storage.sync.set({'currency-type': currencyType}, function() {
   		console.log('Value currently is ' + currencyType);
 	});
 }
 
+// calls change type function when the currency type is selected
 document.getElementById('currencyType').onchange = changeType;
