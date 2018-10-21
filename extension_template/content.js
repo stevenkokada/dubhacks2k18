10b8072c183e7969291688bcf3bc0633310a427a
Altan Haan <altancpp@gmail.com>
2018-10-20 22:31:02 -0700

    fixed recursion problem

--- extension_template/content.js
+++ extension_template/content.js
@@ -1,62 +1,35 @@
-var conversion = {"bags of rice": 300}
-// var elements = document.getElementsByTagName('*');
-//
-// for (var i = 0; i < elements.length; i++) {
-//     var element = elements[i];
-//
-//     for (var j = 0; j < element.childNodes.length; j++) {
-//         var node = element.childNodes[j];
-//
-//         if (true) {
-//             var text = node.nodeValue;
-//             var r = /^\$[0-9]+\.?[0-9]?[0-9]?$/
-//             var original = text.match(r);
-//
-//             if (original != null){
-//
-//
-//             var numeric = parseFloat(original.toString().substring(1)) * conversion["bags of rice"]
-//
-//
-//
-//             var replacedText = text.replace(r, numeric.toString() + " bags of rice");
-//
-//             if (replacedText !== text) {
-//                 element.replaceChild(document.createTextNode(replacedText), node);
-//             }
-//
-//           }
-//         }
-//     }
-// }
-//
+var conversion = {"bags of rice": 1/0.89}
 
-function walkText(node) {
-
-  var conversion = {"bags of rice": 300}
-
+function walkText(node, parent) {
   if (node.nodeType == 3) {
     var text = node.nodeValue;
-    var r = /^\$?[0-9]+(\.[0-9][0-9])?$/;
+    //var r = /($|\s)\$[0-9]+(\.[0-9][0-9])?(\s|$)/;
+    var r = /\b\d+(\.\d\d)?\b/g;
     var original = text.match(r);
 
     if (original != null){
+      console.log(node)
+      console.log(original)
+      var priceText = original.toString().trim();
+      if(priceText.charAt(0) == "$") {
+        priceText = priceText.substring(1);
+      }
 
+      var numeric = parseFloat(priceText) * conversion["bags of rice"]
+      var replacedText = text.replace(r, numeric.toFixed(2).toString() + " bags of rice");
 
-    var numeric = parseFloat(original.toString().substring(1)) * conversion["bags of rice"]
-    var replacedText = text.replace(r, numeric.toString() + " bags of rice");
-
-    if (replacedText !== text) {
-        element.replaceChild(document.createTextNode(replacedText), node);
+      if (replacedText !== text) {
+        parent.replaceChild(document.createTextNode(replacedText), node);
+      }
     }
   }
-}
 
 
   if (node.nodeType == 1 && node.nodeName != "SCRIPT") {
     for (var i = 0; i < node.childNodes.length; i++) {
-      walkText(node.childNodes[i]);
+      walkText(node.childNodes[i], node);
     }
   }
 }
-walkText(document.body);
+
+walkText(document.body, document);
